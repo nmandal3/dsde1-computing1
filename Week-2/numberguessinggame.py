2c8b6cecdf9082fa520b216717c33bec178e420a
nmandal3 <nm819@ic.ac.uk>
2020-05-04 20:04:54 +0100

    space

--- Week-2/numberguessinggame.py
+++ Week-2/numberguessinggame.py
@@ -5,7 +5,7 @@ guess = int(input("Guess a number between 1 and 10: "))
 correct = "Well done, you guessed correctly!: "
 wrong = "Incorrect, try again: "
 while True :
-    if guess == number:
+    if guess == number: 
         print (correct)
         break
     else :
